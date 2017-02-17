870617f83ce299f0ff9016ae1b9086e40c5c0f30
jpdhernandez <jpdhernandez@gmail.com>
2017-02-16 22:12:22 -0500

    Add operations that let you delete and edit notes

--- app/assets/javascript/controllers/noteShowController.js
+++ app/assets/javascript/controllers/noteShowController.js
@@ -1,3 +1,9 @@
-angular.module("NoteTaker").controller("NotesShowController", function(Note, $scope, $routeParams) {
+angular.module("NoteTaker").controller("NotesShowController", function(Note, $scope, $routeParams, $location) {
     $scope.note = Note.get({ id: $routeParams.id });
+
+    $scope.deleteNote = function(note) {
+        note.$remove().finally(function() {
+            $location.path("/");
+        });
+    }
 });
--- app/assets/javascript/services/note.js
+++ app/assets/javascript/services/note.js
@@ -1,3 +1,7 @@
 angular.module("NoteTaker").factory("Note", function($resource) {
-    return $resource("/notes/:id");
+    return $resource("/notes/:id", { id: "@id" }, {
+        update: {
+            method: "PUT"
+        }
+    });
 });
